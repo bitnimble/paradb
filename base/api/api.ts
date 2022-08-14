0716c21fc106a648decfc693a7b27195e81a5f2c
D <me@kumo.dev>
2022-08-15 01:22:17 +1000

    infra: enable brotli compression

--- base/api/api.ts
+++ base/api/api.ts
@@ -96,7 +96,7 @@ export class HttpApi implements Api {
     return new Promise((res, rej) => {
       const xhr = new XMLHttpRequest();
       xhr.open('POST', path(this.apiBase, 'maps', 'submit'), true);
-      xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+      xhr.setRequestHeader('Content-Type', contentType);
       xhr.responseType = 'arraybuffer';
       xhr.upload.addEventListener('progress', onProgress);
       xhr.onload = () => {
@@ -128,9 +128,14 @@ async function get(path: string): Promise<Uint8Array> {
 async function post(path: string, body?: Uint8Array): Promise<Uint8Array> {
   const resp = await fetch(path, {
     method: 'POST',
-    headers: { ['Content-Type']: 'application/octet-stream' },
+    headers: { ['Content-Type']: contentType },
     body,
   });
   const buf = await resp.arrayBuffer();
   return new Uint8Array(buf);
 }
+
+// Set MIME type for requests and responses to 'application/x-protobuf' to allow Cloudflare to use
+// brotli / gzip encoding over the wire. We don't actually use protobuf but Cloudflare doesn't let
+// you force compression for 'application/octet-stream', so this is the closest.
+const contentType = 'application/x-protobuf';
