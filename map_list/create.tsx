a0b6cfe67b1a6edb99bea0b32b0911e54b39cd59
D <me@kumo.dev>
2022-11-13 13:08:07 +1100

    chore: refactor MapListTable jsx from create.tsx into map_list.tsx

--- map_list/create.tsx
+++ map_list/create.tsx
@@ -1,28 +1,8 @@
-import { createTable } from 'base/table/create';
-import { Row } from 'base/table/table';
-import classNames from 'classnames';
-import { action, computed } from 'mobx';
 import { observer } from 'mobx-react';
 import { Api } from 'pages/paradb/base/api/api';
-import metrics from 'pages/paradb/base/metrics/metrics.css';
-import { RouteLink } from 'pages/paradb/base/text/link';
-import { T } from 'pages/paradb/base/text/text';
-import { DifficultyColorPills, MapList } from 'pages/paradb/map_list/map_list';
+import { createMapListTable, MapList } from 'pages/paradb/map_list/map_list';
 import { MapListPresenter, MapListStore } from 'pages/paradb/map_list/map_list_presenter';
-import { routeFor, RoutePath } from 'pages/paradb/router/routes';
-import { PDMap, serializeMap } from 'paradb-api-schema';
 import React from 'react';
-import styles from './map_list.css';
-
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const formatDate = (isoString: string) => {
-  const date = new Date(isoString);
-  const shortMonth = months[date.getMonth()];
-  const maybeYear = date.getFullYear() !== new Date().getFullYear()
-    ? `, ${date.getFullYear()}`
-    : '';
-  return `${shortMonth} ${date.getDate()}${maybeYear}`;
-};
 
 export function createMapList(api: Api) {
   const store = new MapListStore();
@@ -44,74 +24,7 @@ export function createMapList(api: Api) {
     };
   };
 
-  const getRow = (map: PDMap): Row<6> => {
-    const onSelect = action((e: React.MouseEvent<HTMLAnchorElement>) => {
-      captureSkeletonScroll();
-      if (!store.enableBulkSelect) {
-        return;
-      }
-      e.preventDefault();
-      e.stopPropagation();
-      presenter.toggleMapSelection(map.id, e.shiftKey);
-    });
-    const wrapWithMapRoute = (contents: React.ReactNode, additionalClassName?: string) => (
-      <RouteLink
-        additionalClassName={classNames(styles.mapListCell, additionalClassName)}
-        to={routeFor([RoutePath.MAP, map.id])}
-        state={serializeMap(map)}
-        onClick={onSelect}
-      >
-        {contents}
-      </RouteLink>
-    );
-    return {
-      className: classNames({ [styles.mapListRowSelected]: presenter.isSelected(map.id) }),
-      Cells: [
-        React.memo(() => wrapWithMapRoute(<T.Small>{map.title}</T.Small>)),
-        React.memo(() => wrapWithMapRoute(<T.Small>{map.artist}</T.Small>)),
-        React.memo(() => wrapWithMapRoute(<T.Small>{map.author}</T.Small>)),
-        React.memo(() =>
-          wrapWithMapRoute(
-            <DifficultyColorPills difficulties={map.difficulties}/>,
-            styles.centeredCell,
-          )
-        ),
-        React.memo(() => wrapWithMapRoute(<T.Small>{map.favorites}</T.Small>, styles.centeredCell)),
-        React.memo(() =>
-          wrapWithMapRoute(<T.Small>{formatDate(map.submissionDate)}</T.Small>, styles.centeredCell)
-        ),
-      ],
-    };
-  };
-
-  const { store: tableStore, Component: Table } = createTable({
-    data: computed(() => store.maps),
-    columns: [
-      { content: <T.Small weight="bold">Song title</T.Small>, sortLabel: 'title' },
-      { content: <T.Small weight="bold">Artist</T.Small>, sortLabel: 'artist' },
-      { content: <T.Small weight="bold">Mapper</T.Small>, sortLabel: 'author' },
-      {
-        content: <T.Small weight="bold">Difficulties</T.Small>,
-        width: `calc(${metrics.gridBaseline} * 20)`,
-      },
-      {
-        content: <T.Small weight="bold">Favorites</T.Small>,
-        sortLabel: 'favorites',
-        width: `calc(${metrics.gridBaseline} * 20)`,
-      },
-      {
-        content: <T.Small weight="bold">Upload date</T.Small>,
-        sortLabel: 'submissionDate',
-        width: `calc(${metrics.gridBaseline} * 20)`,
-      },
-    ],
-    rowMapper: getRow,
-    onSortChange: presenter.onSortChanged,
-    tableClassname: styles.mapListTable,
-    rowClassname: styles.mapListRow,
-    defaultSortColumn: 5,
-    defaultSortDirection: 'desc',
-  });
+  const { tableStore, Table } = createMapListTable(captureSkeletonScroll, store, presenter);
   presenter.setTableStore(tableStore);
 
   const onMapListMount = () => {
--- map_list/map_list.tsx
+++ map_list/map_list.tsx
@@ -1,10 +1,18 @@
+import { createTable } from 'base/table/create';
+import { Row } from 'base/table/table';
 import classNames from 'classnames';
+import { action, computed } from 'mobx';
 import { observer } from 'mobx-react';
+import metrics from 'pages/paradb/base/metrics/metrics.css';
+import { RouteLink } from 'pages/paradb/base/text/link';
+import { T } from 'pages/paradb/base/text/text';
 import { Button } from 'pages/paradb/base/ui/button/button';
 import { Textbox } from 'pages/paradb/base/ui/textbox/textbox';
 import { getDifficultyColor } from 'pages/paradb/map/map_page';
+import { MapListPresenter, MapListStore } from 'pages/paradb/map_list/map_list_presenter';
 import { searchIcon } from 'pages/paradb/map_list/search_icon';
-import { Difficulty } from 'paradb-api-schema';
+import { routeFor, RoutePath } from 'pages/paradb/router/routes';
+import { Difficulty, PDMap, serializeMap } from 'paradb-api-schema';
 import React from 'react';
 import styles from './map_list.css';
 
@@ -23,6 +31,93 @@ type Props = {
   onSearch(): void,
   onLoadMore(): void,
 };
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const formatDate = (isoString: string) => {
+  const date = new Date(isoString);
+  const shortMonth = months[date.getMonth()];
+  const maybeYear = date.getFullYear() !== new Date().getFullYear()
+    ? `, ${date.getFullYear()}`
+    : '';
+  return `${shortMonth} ${date.getDate()}${maybeYear}`;
+};
+
+export function createMapListTable(
+  captureSkeletonScroll: () => void,
+  store: MapListStore,
+  presenter: MapListPresenter,
+) {
+  const getRow = (map: PDMap): Row<6> => {
+    const onSelect = action((e: React.MouseEvent<HTMLAnchorElement>) => {
+      captureSkeletonScroll();
+      if (!store.enableBulkSelect) {
+        return;
+      }
+      e.preventDefault();
+      e.stopPropagation();
+      presenter.toggleMapSelection(map.id, e.shiftKey);
+    });
+    const wrapWithMapRoute = (contents: React.ReactNode, additionalClassName?: string) => (
+      <RouteLink
+        additionalClassName={classNames(styles.mapListCell, additionalClassName)}
+        to={routeFor([RoutePath.MAP, map.id])}
+        state={serializeMap(map)}
+        onClick={onSelect}
+      >
+        {contents}
+      </RouteLink>
+    );
+    return {
+      className: classNames({ [styles.mapListRowSelected]: presenter.isSelected(map.id) }),
+      Cells: [
+        React.memo(() => wrapWithMapRoute(<T.Small>{map.title}</T.Small>)),
+        React.memo(() => wrapWithMapRoute(<T.Small>{map.artist}</T.Small>)),
+        React.memo(() => wrapWithMapRoute(<T.Small>{map.author}</T.Small>)),
+        React.memo(() =>
+          wrapWithMapRoute(
+            <DifficultyColorPills difficulties={map.difficulties}/>,
+            styles.centeredCell,
+          )
+        ),
+        React.memo(() => wrapWithMapRoute(<T.Small>{map.favorites}</T.Small>, styles.centeredCell)),
+        React.memo(() =>
+          wrapWithMapRoute(<T.Small>{formatDate(map.submissionDate)}</T.Small>, styles.centeredCell)
+        ),
+      ],
+    };
+  };
+
+  const { store: tableStore, Component } = createTable({
+    data: computed(() => store.maps),
+    columns: [
+      { content: <T.Small weight="bold">Song title</T.Small>, sortLabel: 'title' },
+      { content: <T.Small weight="bold">Artist</T.Small>, sortLabel: 'artist' },
+      { content: <T.Small weight="bold">Mapper</T.Small>, sortLabel: 'author' },
+      {
+        content: <T.Small weight="bold">Difficulties</T.Small>,
+        width: `calc(${metrics.gridBaseline} * 20)`,
+      },
+      {
+        content: <T.Small weight="bold">Favorites</T.Small>,
+        sortLabel: 'favorites',
+        width: `calc(${metrics.gridBaseline} * 20)`,
+      },
+      {
+        content: <T.Small weight="bold">Upload date</T.Small>,
+        sortLabel: 'submissionDate',
+        width: `calc(${metrics.gridBaseline} * 20)`,
+      },
+    ],
+    rowMapper: getRow,
+    onSortChange: presenter.onSortChanged,
+    tableClassname: styles.mapListTable,
+    rowClassname: styles.mapListRow,
+    defaultSortColumn: 5,
+    defaultSortDirection: 'desc',
+  });
+
+  return { tableStore, Table: Component };
+}
 
 export const DifficultyColorPills = (props: { difficulties: Difficulty[] }) => (
   <div className={styles.difficulties}>
