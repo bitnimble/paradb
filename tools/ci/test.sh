a208cf1d7e678a6386ea7ffe25988a659a6d4413
D <me@kumo.dev>
2023-09-26 02:51:43 +1000

    .

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -11,15 +11,17 @@ cp .env.test .env
 buildkite-agent artifact download .next.tar.gz .
 tar -xzf .next.tar.gz
 
-yarn start &
-nohup meilisearch --no-analytics --master-key="123" > /dev/null 2>&1 &
+{
+  yarn start &
+  nohup meilisearch --no-analytics --master-key="123" > /dev/null 2>&1 &
 
-nohup minio server /data > /dev/null 2>&1 &
-yarn test
-
-pkill -f "sh -c next start"
-pkill -f ".bin/next start"
-pkill -f "next-router-worker"
-pkill -f "next/dist/compiled"
-pkill -f "meilisearch"
-pkill -f "minio"
+  nohup minio server /data > /dev/null 2>&1 &
+  yarn test
+} || {
+  pkill -f "sh -c next start" || true
+  pkill -f ".bin/next start" || true
+  pkill -f "next-router-worker" || true
+  pkill -f "next/dist/compiled" || true
+  pkill -f "meilisearch" || true
+  pkill -f "minio" || true
+}
