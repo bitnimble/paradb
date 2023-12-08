4f90276b18a5e7a72b37341575dbda4bcaa252ff
D <me@kumo.dev>
2023-12-09 02:03:52 +1100

    fix: adjust sentry trace rate

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,5 +2,5 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: 'https://5fcfeb49d9b34cf1ac9e6e705856fd5c@o951496.ingest.sentry.io/5900440',
-  tracesSampleRate: 0.2,
+  tracesSampleRate: 0.05,
 });
