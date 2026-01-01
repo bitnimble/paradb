f7b431f0af236252c3b75cffc263d088afe2c60e
D <me@kumo.dev>
2026-01-01 12:11:05 +1100

    fix: make db pool instantiation synchronous

--- src/services/db/pool.ts
+++ src/services/db/pool.ts
@@ -7,36 +7,35 @@ const db: {
 
 export async function getDbPool(maxConnections?: number) {
   if (!db.pool) {
-    db.pool = await initPool(maxConnections);
+    db.pool = initPool(maxConnections);
+    try {
+      // Quickly test connection to check that DB is running
+      const client = await db.pool.connect();
+      client.release();
+    } catch (e) {
+      const envVars = getEnvVars();
+      throw new Error(
+        'Could not connect to database, is it running? Connection string: ' +
+          `postgresql://${envVars.pgUser}:<password>@${envVars.pgHost}:${envVars.pgPort}/${envVars.pgDatabase}\n${e}`
+      );
+    }
   }
   return db.pool;
 }
 
-async function initPool(maxConnections?: number) {
+function initPool(maxConnections?: number) {
   console.log('Initialising pg db pool connection');
   // Test DB
   const envVars = getEnvVars();
-  try {
-    db.pool = new pg.Pool({
-      host: envVars.pgHost,
-      port: envVars.pgPort,
-      database: envVars.pgDatabase,
-      user: envVars.pgUser,
-      password: envVars.pgPassword,
-      min: maxConnections,
-      max: maxConnections,
-    });
-    db.pool.on('error', (err) => console.error(err));
-
-    // Quickly test connection to check that DB is running
-    const client = await db.pool.connect();
-    client.release();
-  } catch (e) {
-    throw new Error(
-      'Could not connect to database, is it running? Connection string: ' +
-        `postgresql://${envVars.pgUser}:${envVars.pgPassword}@${envVars.pgHost}:${envVars.pgPort}/${envVars.pgDatabase}`
-    );
-  }
-
+  db.pool = new pg.Pool({
+    host: envVars.pgHost,
+    port: envVars.pgPort,
+    database: envVars.pgDatabase,
+    user: envVars.pgUser,
+    password: envVars.pgPassword,
+    min: maxConnections,
+    max: maxConnections,
+  });
+  db.pool.on('error', (err) => console.error(err));
   return db.pool;
 }
